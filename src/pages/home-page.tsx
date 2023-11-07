38be40a8b4a2dc839a4f281793253aa78969340a
beatrice2802 <126410861+beatrice2802@users.noreply.github.com>
2023-11-07 20:01:11 +0200

    login page

---
+++ src/pages/home-page.tsx
@@ -0,0 +1,47 @@
+import {Button, Container, TextField, Typography} from '@mui/material';
+import {CSSProperties, useState} from "react";
+// import { useNavigate } from 'react-router-dom';
+export const HomePage=()=>{
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    // const navigate = useNavigate();
+    const handleLogin=()=>{
+        // navigate('/donations')   //TODO
+    }
+    const handleEmail = (event: any) => {
+        setEmail(event.target.value);
+    }
+    const handlePassword = (event: any) => {
+        setPassword(event.target.value);
+    }
+    return(
+        <>
+            <Container sx={textWrapperStyle}>
+                <Typography variant={'h3'} > Hi!</Typography>
+
+                <Typography variant={'h3'}>This is help hub</Typography>
+                <TextField
+                value={email}
+                label='email'
+                onChange={handleEmail}
+
+                />
+                <TextField
+                    value={password}
+                    label='password'
+                    onChange={handlePassword}
+
+                />
+                <Button onClick={handleLogin}>Log in</Button>
+            </Container>
+        </>
+    )
+}
+const textWrapperStyle: CSSProperties = {
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    height: '70vh',
+};
