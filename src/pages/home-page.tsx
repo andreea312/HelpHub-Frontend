a55c00f1a51eb87848ac22e6772732d4643970f7
andreea-312 <andre.negoitescu@gmail.com>
2023-12-11 18:20:42 +0200

    passwords display

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,120 +1,165 @@
-import { Button, Container, TextField, Typography } from '@mui/material';
-import { CSSProperties, useContext, useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { Button, Container, TextField, Typography, IconButton, InputAdornment } from '@mui/material';
+import { CSSProperties, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../auth/AuthProvider';
-import background from "./fundal.png";
-import 'google-fonts'
+import background from './fundal.png';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
-export const HomePage=()=>{
-    const { login, logout, user, isAuthenticated } = useContext(AuthContext);
+export const HomePage = () => {
+  const { login, user } = useContext(AuthContext);
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(()=> {
-        if(user.id){
-            navigate('/causes');   
-        }
-    }, [user.id]);
-
-    const handleLogin= () => {
-        console.log('login!');
-        login?.(email, password); 
+  useEffect(() => {
+    if (user.id) {
+      navigate('/causes');
     }
-    const handleRegisterNav= () => {
-        navigate('/register');   
-    }
-    const handleEmail = (event: any) => {
-        setEmail(event.target.value);
-    }
-    const handlePassword = (event: any) => {
-        setPassword(event.target.value);
-    }
-    return(
-        <>
-        <div style={wrapperStyle1}>
-            <Container sx={wrapperStyleLeft}>
-                
-                <Typography variant={'h2'} sx={{ marginTop: '390px', color: '#551daa'}}>we care.</Typography>
-                
-                <Typography variant={'h2'} sx={{ marginTop: '30px', color: '#551daa' }}>we support.</Typography>
-                
-                <Typography variant={'h2'} sx={{ marginTop: '30px', color: '#551daa' }}>we change lives.</Typography>
-            
-            </Container>
-            <Container sx={wrapperStyle2}>
-                <Typography variant={'h3'} sx={{ fontFamily: 'Pacifico, cursive' }}>HelpHub</Typography>
-                <Typography variant={'h4'}>Login</Typography>
-                <br></br>
-                <br></br>
-                <br></br>
-                <TextField
-                value={email}
-                label='email'
-                onChange={handleEmail}
-   
-                />
-                <br></br>
-                <TextField
-                    value={password}
-                    label='password'
-                    onChange={handlePassword}
-                    type='password' 
-                />
-                <br></br>
-                <Button sx={{ background: '#9999ff','&:hover': {
-                        backgroundColor: '#ccccff',
-                    }, color: 'black',  textTransform: 'none', width: '30%', fontFamily: 'Pacifico, cursive'}} onClick={handleLogin}>Log in</Button>
-                <br></br>
-                <Button sx={{ background: '#9999ff','&:hover': {
-                        backgroundColor: '#ccccff',
-                    }, color: 'black', textTransform: 'none', width: '30%', fontFamily: 'Pacifico, cursive'}} onClick={handleRegisterNav}>Register</Button>
-            </Container>
-            <Container sx={wrapperStyleRight}>
-                
-            </Container>
-        </div>
-        </>
-        
-    )
-}
+  }, [user.id]);
 
+  const handleLogin = () => {
+    console.log('login!');
+    login?.(email, password);
+  };
+
+  const handleRegisterNav = () => {
+    navigate('/register');
+  };
+
+  const handleEmail = (event: any) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePassword = (event: any) => {
+    setPassword(event.target.value);
+  };
+
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  return (
+    <>
+      <div style={wrapperStyle1}>
+        <Container sx={wrapperStyleLeft}>
+          <Typography variant={'h2'} sx={{ marginTop: '390px', color: '#551daa' }}>
+            we care.
+          </Typography>
+          <Typography variant={'h2'} sx={{ marginTop: '30px', color: '#551daa' }}>
+            we support.
+          </Typography>
+          <Typography variant={'h2'} sx={{ marginTop: '30px', color: '#551daa' }}>
+            we change lives.
+          </Typography>
+        </Container>
+        <Container sx={wrapperStyle2}>
+          <Typography variant={'h3'} sx={{ fontFamily: 'Pacifico, cursive' }}>
+            HelpHub
+          </Typography>
+          <Typography variant={'h4'}>Login</Typography>
+          <br />
+          <br />
+          <br />
+          <TextField
+            value={email}
+            label="email"
+            onChange={handleEmail}
+            sx={{ width: '73%' }}
+          />
+          <br />
+          <TextField
+            value={password}
+            label="password"
+            onChange={handlePassword}
+            sx={{ width: '73%' }}
+            type={showPassword ? 'text' : 'password'}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={handleTogglePasswordVisibility} edge="end">
+                    {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <br />
+          <Button
+            sx={{
+              background: '#9999ff',
+              '&:hover': {
+                backgroundColor: '#ccccff',
+              },
+              color: 'black',
+              textTransform: 'none',
+              width: '30%',
+              fontFamily: 'Pacifico, cursive',
+            }}
+            onClick={handleLogin}
+          >
+            Log in
+          </Button>
+          <br />
+          <Button
+            sx={{
+              background: '#9999ff',
+              '&:hover': {
+                backgroundColor: '#ccccff',
+              },
+              color: 'black',
+              textTransform: 'none',
+              width: '30%',
+              fontFamily: 'Pacifico, cursive',
+            }}
+            onClick={handleRegisterNav}
+          >
+            Register
+          </Button>
+        </Container>
+        <Container sx={wrapperStyleRight}>{/* ... */}</Container>
+      </div>
+    </>
+  );
+};
 
 const wrapperStyle1: CSSProperties = {
-    margin: '0px',
-    padding: '0px',
-    backgroundImage: `url(${background})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    display: 'flex',
+  margin: '0px',
+  padding: '0px',
+  backgroundImage: `url(${background})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  display: 'flex',
 };
 
 const wrapperStyle2: CSSProperties = {
-    backgroundColor: 'transparent',
-    backdropFilter: 'blur(5px)',
-    alignItems: 'center',
-    justifyContent: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-    height: '100vh',
-    width: '50vh',
-    border: '1px solid black',
-    borderRadius: '15px',
-    padding: '16px',
-    margin: '0px'
+  backgroundColor: 'transparent',
+  backdropFilter: 'blur(5px)',
+  alignItems: 'center',
+  justifyContent: 'center',
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100vh',
+  width: '50vh',
+  border: '1px solid black',
+  borderRadius: '15px',
+  padding: '16px',
+  margin: '0px',
 };
 
 const wrapperStyleLeft: CSSProperties = {
-    backgroundColor: 'transparent',
-    height: '100vh',
-    margin: '0px',
-    width: '38.5%'
+  backgroundColor: 'transparent',
+  height: '100vh',
+  margin: '0px',
+  width: '38.5%',
 };
 const wrapperStyleRight: CSSProperties = {
-    backgroundColor: 'transparent',
-    height: '100vh',
-    margin: '0px',
-    width: '38.5%'
+  backgroundColor: 'transparent',
+  height: '100vh',
+  margin: '0px',
+  width: '38.5%',
 };
--- src/pages/register-page.tsx
+++ src/pages/register-page.tsx
@@ -1,149 +1,182 @@
-import { Button, Container, Select, TextField, Typography } from '@mui/material';
-import { CSSProperties, useContext, useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { Button, Container, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
+import { CSSProperties, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UsersContext } from '../auth/RegisterProvider';
-import { RegisteredUser } from "../shared/Types";
-import { Label } from '@mui/icons-material';
-import background from "./fundal.png";
+import { RegisteredUser } from '../shared/Types';
+import background from './fundal.png';
 
-
-
-export const RegisterPage=()=>{
+export const RegisterPage = () => {
     const { addUser } = useContext(UsersContext);
     const [email, setEmail] = useState('');
     const [parola, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     const [username, setUsername] = useState('');
     const [fullName, setFullName] = useState('');
     const [gender, setGender] = useState('');
+    const [passwordsMatch, setPasswordsMatch] = useState(true); // Track if passwords match
     const navigate = useNavigate();
-
-
+  
     const handleRegister = async () => {
-        try{
-            const user: RegisteredUser = {
-                username: username,
-                email: email,
-                parola: parola,
-                fullName: fullName,
-                gender: parseInt(gender),
-                coins: 100,
-                level: 1,
-            }
-            console.log(user);
-            await addUser?.(user);
-            console.log('added user!!');
-            navigate('/');
-        } catch(error: any){
-            console.log('error: '+ error);
+      try {
+        if (parola !== confirmPassword) {
+          console.error('Passwords do not match');
+          setPasswordsMatch(false);
+          return;
         }
-    }
+  
+        const user: RegisteredUser = {
+          username: username,
+          email: email,
+          parola: parola,
+          fullName: fullName,
+          gender: parseInt(gender),
+          coins: 100,
+          level: 1,
+        };
+  
+        console.log(user);
+        await addUser?.(user);
+        console.log('added user!!');
+        navigate('/');
+      } catch (error: any) {
+        console.log('error: ' + error);
+      }
+    };
+  
     const handleUsername = (event: any) => {
-        setUsername(event.target.value);
-    }
+      setUsername(event.target.value);
+    };
+  
     const handleEmail = (event: any) => {
-        setEmail(event.target.value);
-    }
+      setEmail(event.target.value);
+    };
+  
     const handlePassword = (event: any) => {
-        setPassword(event.target.value);
-    }
+      setPassword(event.target.value);
+      // Reset the error state when typing in the password field
+      setPasswordsMatch(true);
+    };
+  
+    const handleConfirmPassword = (event: any) => {
+      setConfirmPassword(event.target.value);
+      // Validate passwords match and set the error state
+      setPasswordsMatch(parola === event.target.value);
+    };
+  
     const handleFullName = (event: any) => {
-        setFullName(event.target.value);
-    }
+      setFullName(event.target.value);
+    };
+  
     const handleGender = (event: any) => {
-        setGender(event.target.value);
-    }
-    return(
-        <>
+      setGender(event.target.value);
+    };
+  
+    return (
+      <>
         <div style={wrapperStyle1}>
-            <Container sx={wrapperStyle2}>
-                <Typography variant={'h3'} sx={{ fontFamily: 'Pacifico, cursive' }}>HelpHub</Typography>
-                <Typography variant={'h4'}>Register</Typography>
-                <br></br>
+          <Container sx={wrapperStyle2}>
+            <Typography variant={'h3'} sx={{ fontFamily: 'Pacifico, cursive' }}>
+              HelpHub
+            </Typography>
+            <Typography variant={'h4'}>Register</Typography>
+            <br />
+            <TextField
+              value={email}
+              label="email"
+              onChange={handleEmail}
+            />
+            <br />
+            <TextField
+              value={username}
+              label="username"
+              onChange={handleUsername}
+            />
+            <br />
+            <TextField
+              value={parola}
+              label="password"
+              onChange={handlePassword}
+              type="password"
+            />
+            <br />
 
-                <TextField
-                value={email}
-                label='email'
-                onChange={handleEmail}
-
-                />
-                                <br></br>
-
-                <TextField
-                value={username}
-                label='username'
-                onChange={handleUsername}
-
-                />
-                                <br></br>
-
-                <TextField
-                    value={parola}
-                    label='password'
-                    onChange={handlePassword}
-                    type='password'
-                />
-                                <br></br>
-
-                <TextField
-                value={fullName}
-                label='full name'
-                onChange={handleFullName}
-
-                />
-                                 <br></br>
-                Gender
-                <Select 
-                    sx={{width: '70%'}}
-                    name="gender"
-                    value={gender}
-                    onChange={handleGender}
-                    >
-                    <option value="0" style={{  background: '#ccccff'}}>Male</option>
-                    <option value="1" style={{ background: '#ccccff'}}>Female</option>
-                    <option value="2" style={{ background: '#ccccff'}}>Other</option>
-                </Select>
-
-                <br></br>
-
-                <Button sx={{ background: '#9999ff','&:hover': {
-                        backgroundColor: '#ccccff',
-                    }, color: 'black',  textTransform: 'none', width: '30%', fontFamily: 'Pacifico, cursive'}} onClick={handleRegister}>Register</Button>
-            </Container>
+            <TextField
+              value={confirmPassword}
+              label="confirm password"
+              onChange={handleConfirmPassword}
+              type="password"
+              error={!passwordsMatch}
+              helperText={passwordsMatch ? '' : 'Passwords do not match'}
+            />
+            <br />
+            <TextField
+              value={fullName}
+              label="full name"
+              onChange={handleFullName}
+            />
+            <br />
+            <FormControl margin="dense" variant="outlined" sx={{ width: '73%' }}>
+              <InputLabel>gender</InputLabel>
+              <Select label="Gender" value={gender} onChange={handleGender}>
+                <MenuItem value="0">Male</MenuItem>
+                <MenuItem value="1">Female</MenuItem>
+                <MenuItem value="2">Other</MenuItem>
+              </Select>
+            </FormControl>
+            <br />
+            <Button
+              sx={{
+                background: '#9999ff',
+                '&:hover': {
+                  backgroundColor: '#ccccff',
+                },
+                color: 'black',
+                textTransform: 'none',
+                width: '30%',
+                fontFamily: 'Pacifico, cursive',
+              }}
+              onClick={handleRegister}
+            >
+              Register
+            </Button>
+          </Container>
         </div>
-        </>
-    )
-}
+      </>
+    );
+  };
+
 const wrapperStyle1: CSSProperties = {
-    margin: '0px',
-    padding: '0px',
-    backgroundImage: `url(${background})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
+  margin: '0px',
+  padding: '0px',
+  backgroundImage: `url(${background})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
 };
 
 const wrapperStyle2: CSSProperties = {
-    backgroundColor: 'transparent',
-    backdropFilter: 'blur(5px)',
-    alignItems: 'center',
-    justifyContent: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-    height: '100vh',
-    width: '50vh',
-    border: '1px solid black',
-    borderRadius: '15px',
-    padding: '16px',
+  backgroundColor: 'transparent',
+  backdropFilter: 'blur(5px)',
+  alignItems: 'center',
+  justifyContent: 'center',
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100vh',
+  width: '50vh',
+  border: '1px solid black',
+  borderRadius: '15px',
+  padding: '16px',
 };
 
 const wrapperStyleLeft: CSSProperties = {
-    backgroundColor: 'transparent',
-    height: '100vh',
-    margin: '0px',
-    width: '38.5%'
+  backgroundColor: 'transparent',
+  height: '100vh',
+  margin: '0px',
+  width: '38.5%',
 };
 const wrapperStyleRight: CSSProperties = {
-    backgroundColor: 'transparent',
-    height: '100vh',
-    margin: '0px',
-    width: '38.5%'
+  backgroundColor: 'transparent',
+  height: '100vh',
+  margin: '0px',
+  width: '38.5%',
 };
